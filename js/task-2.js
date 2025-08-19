ea8cff3941e3cb6f6cdba22c951ffc452485e69e
m.ruban <m.ruban@testportal.gov.ua>
2025-08-19 14:31:21 +0200

    init commit

---
+++ js/task-2.js
@@ -0,0 +1,8 @@
+class Storage { }
+
+const storage = new Storage(['Nanitoids', 'Prolonger', 'Antigravitator']);
+console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator"]
+storage.addItem('Droid');
+console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator", "Droid"]
+storage.removeItem('Prolonger');
+console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
