1ff0e9969983bb5bc1618474aa116a2ac7719efe
m.ruban <m.ruban@testportal.gov.ua>
2025-08-19 15:42:02 +0200

    solve task-3

--- js/task-3.js
+++ js/task-3.js
@@ -1,4 +1,25 @@
-class StringBuilder { }
+class StringBuilder {
+    #value;
+    constructor(value) {
+        this.#value = value;
+    }
+
+    getValue() {
+        return this.#value;
+    }
+
+    padStart(padding) {
+        this.#value = `${padding}${this.#value}`;
+    }
+
+    padEnd(padding) {
+        this.#value = `${this.#value}${padding}`;
+    }
+
+    padBoth(padding) {
+        this.#value = `${padding}${this.#value}${padding}`;
+    }
+}
 
 const builder = new StringBuilder('.');
 console.log(builder.getValue()); // "."
